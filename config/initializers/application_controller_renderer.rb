3a2e6840086691a60fe5fec91508581012b33521
森本拓見 <morimototakumi@MorimotoTakuminoMacBook-Pro.local>
2020-02-02 04:01:51 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '0711d5842e2cd1eb04f424250046e0b424ba29a1ac64f051ba4dd417126a2738ec629b2f3c70bb6a85fcbfe883800f2d601223b9aa887dbce58fe18a0794ff57'
